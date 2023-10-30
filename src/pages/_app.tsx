714ae95c659c037a811aba5a74db9c779821ad34
João Antônio <joaoasccc@gmail.com>
2023-10-30 17:33:13 -0300

    v1

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import '../styles/global.scss'
+
+import { Header } from '../components/Header'
+import { Player } from '../components/Player'
+
+import styles from '../styles/app.module.scss'
+
+
+export default function App({ Component, pageProps }) {
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header/>
+        <Component {...pageProps} />
+      </main>
+      <Player/>
+    </div>
+  )
+}
